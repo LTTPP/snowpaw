3658336202659f7dc7c60b05539b8a80c5e884b4
jianliuwei <liujianwei.me@gmail.com>
2021-01-24 17:13:02 +0800

    increase version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="snowpaw",
-    version="0.0.1",
+    version="0.0.2",
     author="Liu Jianwei",
     author_email="tungliu@126.com",
     description="A useful python package",

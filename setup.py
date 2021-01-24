900b8f8c226c27f3d641e209359cf7ee2ea21033
jianliuwei <liujianwei.me@gmail.com>
2021-01-24 21:25:20 +0800

    impot * in __init__.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="snowpaw",
-    version="0.0.2",
+    version="0.0.3",
     author="Liu Jianwei",
     author_email="tungliu@126.com",
     description="A useful python package",
